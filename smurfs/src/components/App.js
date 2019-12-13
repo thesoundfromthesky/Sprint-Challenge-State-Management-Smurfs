9953c7f1dad4cab72da4ac4a6c1a3054991d4c1d
thesoundfromthesky <nagara@naver.com>
2019-12-13 11:11:33 -0600

    redux post

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
+import Display from "./Display";
 
 function App() {
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.smurfReducer);
-  console.log(data);
   return (
     <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+      <Display />
     </div>
   );
 }
