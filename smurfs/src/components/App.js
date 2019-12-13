c39179f3fed59cd138b665e77c0db234bbc9c575
thesoundfromthesky <nagara@naver.com>
2019-12-13 12:05:05 -0600

    form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Display from "./Display";
+import SmurfForm from "./SmurfForm";
 
 function App() {
   return (
     <div className="App">
+      <SmurfForm />
       <Display />
     </div>
   );
